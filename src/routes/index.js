f0286800871d1707f0ce902141fd6f931cc73799
NicolasCorredor02 <nicolas.d02@outlook.com>
2025-04-06 00:38:25 -0500

    Feat: Routes for user added

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import products from 'root/routes/product.route.js'
 import carts from 'root/routes/cart.route.js'
 import admin from 'root/routes/admin.routes.js'
+import user from 'root/routes/user.route.js'
 import { productController } from 'root/controller/user/product.controller.js'
 
 const router = Router()
@@ -13,6 +14,7 @@ router.get('/', productController.getAll)
 
 router.use('/api/clients/products', products)
 router.use('/api/clients/cart', carts)
+router.use('/api/clients/user', user)
 router.use('/api/admin', admin)
 
 export default router
