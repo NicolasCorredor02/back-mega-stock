bcd398349404a999bfdd29b903249bffcfd321ea
NicolasCorredor02 <nicolas.d02@outlook.com>
2025-04-11 22:29:59 -0500

    Fix: Prompt error response disabled to don't see error in views

--- src/middlewares/passportCall.js
+++ src/middlewares/passportCall.js
@@ -2,19 +2,16 @@ import passport from 'passport'
 
 export const passportCall = (strategy, options = {}) => {
   return async (req, res, next) => {
-    passport.authenticate(strategy, options, (error, user, info) => {
+    passport.authenticate(strategy, { ...options, session: false }, (error, user, info) => {
       if (error) return next(error)
-      if (!user) {
-        return res.status(401).send({
-          status: 'error',
-          message: info.message ? info.message : info.toString()
-        })
-      }
-      req.login(user, (err) => {
-        if (err) return next(err)
-        req.user = user
-        next()
-      })
+      // if (!user) {
+      //   return res.status(401).send({
+      //     status: 'error',
+      //     message: info && info.message ? info.message : info.toString()
+      //   })
+      // }
+      req.user = user
+      next()
     })(req, res, next)
   }
 }
