a7e89b7c475b1f2ed51a81a865c2b7a61d6fca5c
NicolasCorredor02 <nicolas.d02@outlook.com>
2025-04-11 22:31:40 -0500

    Feat: Strategies implemented for user's and admin's routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -13,6 +13,7 @@ const router = Router()
 // Post product into DB
 router.post(
   '/products/product/add',
+  passportCall('jwt-cookies'),
   isAuthAdmin, // Middleware para validar si el usuario en session actual corresponde al admin
   uploadProductImages, // Se añade el middleware de upload de la configuraion de multer para trabajar con las imagenes de los productos
   handleErrorUploads, // Se agrega el middleware para validar los files que se suben como imagen del producto
@@ -22,6 +23,7 @@ router.post(
 // Put product to update
 router.put(
   '/products/product/update/:pid',
+  passportCall('jwt-cookies'),
   isAuthAdmin,
   uploadProductImages, // Se añade el middleware de upload de la configuraion de multer para trabajar con las imagenes de los productos (max 5)
   handleErrorUploads, // Se agrega el middleware para validar los files que se suben como imagen del producto
@@ -32,41 +34,42 @@ router.put(
 // router.delete('/products/product/delete', productController.changeStatus)
 
 // Delete product from DB
-router.delete('/products/product/delete', isAuthAdmin, productController.delete)
+router.delete('/products/product/delete', passportCall('jwt-cookies'), isAuthAdmin, productController.delete)
 
 // Get product by ID
-router.get('/products/product/:pid', isAuthAdmin, productController.getById)
+router.get('/products/product/:pid', passportCall('jwt-cookies'), isAuthAdmin, productController.getById)
 
 // Get all products
-router.get('/products', isAuthAdmin, productController.getAll)
+router.get('/products', passportCall('jwt-cookies'), isAuthAdmin, productController.getAll)
 
 //* --------------- Admin carts ------------------------
 // Post create a cart
-router.post('/carts/cart/create', isAuthAdmin, cartController.create)
+router.post('/carts/cart/create', passportCall('jwt-cookies'), isAuthAdmin, cartController.create)
 
 // Put para actualizar la data de un carrito
-router.put('/carts/cart/update/:cid', isAuthAdmin, cartController.update)
+router.put('/carts/cart/update/:cid', passportCall('jwt-cookies'), isAuthAdmin, cartController.update)
 
 // Delete cart by id
-router.delete('/carts/cart/delete', isAuthAdmin, cartController.delete)
+router.delete('/carts/cart/delete', passportCall('jwt-cookies'), isAuthAdmin, cartController.delete)
 
 // Get cart by ID
-router.get('/carts/cart/:cid', isAuthAdmin, cartController.getById)
+router.get('/carts/cart/:cid', passportCall('jwt-cookies'), isAuthAdmin, cartController.getById)
 
 // Get all Carts
-router.get('/carts', isAuthAdmin, cartController.getAll)
+router.get('/carts', passportCall('jwt-cookies'), isAuthAdmin, cartController.getAll)
 
 //* --------------- Admin users ------------------------
 // POST register user
 router.post('/users/user/register',
+  passportCall('jwt-cookies'),
   isAuthAdmin,
   uploadUserImages,
   handleErrorUploads,
-  passportCall('register'), // Implementacion del middleware de strategy para validar la session
   userController.register)
 
 // PUT update user
 router.put('/users/user/update/:uid',
+  passportCall('jwt-cookies'),
   isAuthAdmin,
   uploadUserImages,
   handleErrorUploads,
@@ -77,16 +80,16 @@ router.put('/users/user/update/:uid',
 // router.delete('/users/user/delete', isAuthAdminLogin, userController.changeStatus)
 
 // Delete user
-router.delete('/users/user/delete', isAuthAdmin, userController.delete)
+router.delete('/users/user/delete', passportCall('jwt-cookies'), isAuthAdmin, userController.delete)
 
 // Get User by ID
-router.get('/users/user/:uid', isAuthAdmin, userController.getById)
+router.get('/users/user/:uid', passportCall('jwt-cookies'), isAuthAdmin, userController.getById)
 
 // Get all users
-router.get('/users', isAuthAdmin, userController.getAll)
+router.get('/users', passportCall('jwt-cookies'), isAuthAdmin, userController.getAll)
 
 // Get admin settings
-router.get('/settings', isAuthAdmin, (req, res, next) => {
+router.get('/settings', passportCall('jwt-cookies'), isAuthAdmin, (req, res, next) => {
   try {
     return res.render('adminSettings')
   } catch (error) {
@@ -97,15 +100,14 @@ router.get('/settings', isAuthAdmin, (req, res, next) => {
 // Post para iniciar sesion como admin
 router.post(
   '/login',
-  passportCall('loginAdmin'), // Implementacion del middleware de strategy para validar el inicio de sesion
   userController.loginAdmin
 )
 
 // Get para cerrar session
-router.get('/logout', isAuthAdmin, userController.logOut)
+router.get('/logout', passportCall('jwt-cookies'), isAuthAdmin, userController.logOut)
 
 // Validator for admin
-router.get('/', isNotAuthAdmin, (req, res, next) => {
+router.get('/', passportCall('jwt-cookies'), isNotAuthAdmin, (req, res, next) => {
   try {
     return res.render('adminLogin')
   } catch (error) {
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -12,12 +12,12 @@ router.post(
   '/register',
   uploadUserImages,
   handleErrorUploads,
-  passportCall('register'), // Implementacion del middleware de strategy para validar la session
   userController.register
 )
 
 // Put to update a user
 router.put('/update/:uid',
+  passportCall('jwt-cookies'),
   isAuth,
   uploadUserImages,
   handleErrorUploads,
@@ -25,29 +25,34 @@ router.put('/update/:uid',
 )
 
 // Delete to soft delete of user
-router.delete('/delete', isAuth, userController.changeStatus)
+router.delete('/delete', passportCall('jwt-cookies'), isAuth, userController.changeStatus)
 
 // Get by Id
-router.get('/profile/:uid', isAuth, userController.getById)
+router.get('/profile', passportCall('jwt-cookies'), isAuth, userController.getById)
 
 // User login
 router.post(
   '/login',
-  passportCall('login'), // Implementacion del middleware de strategy para validar la session
   userController.login
 )
 
 router.get(
   '/login/auth/google',
-  passportCall('google', { scope: ['profile', 'email'] }), // Implementacion del middleware de strategy para validar la session con Google
-  userController.login
+  passportCall('google', { scope: ['profile', 'email'] }) // Implementacion del middleware de strategy para iniciar el flujo de session con Google
 )
 
-// Log out session
-router.get('/logout', isAuth, userController.logOut)
+// Callback para Google Auth
+router.get(
+  '/login/auth/google/callback',
+  passportCall('google', { failureRedirect: '/' }), // Implementacion del middleware de strategy para validar la session con Google
+  userController.loginGoogle
+)
+
+// Log out
+router.get('/logout', passportCall('jwt-cookies'), isAuth, userController.logOut)
 
 // Get para render de form para register
-router.get('/register', isNotAuth, (req, res, next) => {
+router.get('/register', passportCall('jwt-cookies'), isNotAuth, (req, res, next) => {
   try {
     return res.render('userRegister')
   } catch (error) {
@@ -56,7 +61,7 @@ router.get('/register', isNotAuth, (req, res, next) => {
 })
 
 // Get by email
-router.get('/', isNotAuth, (req, res, next) => {
+router.get('/', passportCall('jwt-cookies'), isNotAuth, (req, res, next) => {
   try {
     return res.render('userLogin')
   } catch (error) {
