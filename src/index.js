24fc9d3fb6ae53769fb49c779960d94e995eae4b
NicolasCorredor02 <nicolas.d02@outlook.com>
2025-04-16 23:09:05 -0500

    Feat: Add port to env example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import serverUp from 'root/config/server.js'
+import 'dotenv/config'
 
 const server = await serverUp()
-const PORT = 8080
+const PORT = process.env.PORT
 
 server.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`)
