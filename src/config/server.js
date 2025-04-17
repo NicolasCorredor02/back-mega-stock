3f10393c446626b8766d33d5f760f619292f6bdd
NicolasCorredor02 <nicolas.d02@outlook.com>
2025-04-16 19:05:45 -0500

    Provitional commit

--- src/config/server.js
+++ src/config/server.js
@@ -27,12 +27,11 @@ const serverUp = async () => {
   handlebarsConfig(app)
 
   //* Middlewares
-  app.use(express.json()); // Ingreso de data por el body de HTTP
-  app.use(express.urlencoded({ extended: true })); // Ingreso de data de forms que sean extensos y requieran una inspeccion profunda
-  app.use(logger("dev"));
-  app.use("/static", express.static(path.join(srcPath, "public"))); // Implementacion de middleware para establecer un directorio static que alojara los archivos public para el render desde server
-  app.use("/uploads", express.static(path.join(srcPath, "uploads"))); // Implementacion para establecer directorio estatico de las imagenes que se suben
-  
+  app.use(express.json()) // Ingreso de data por el body de HTTP
+  app.use(express.urlencoded({ extended: true })) // Ingreso de data de forms que sean extensos y requieran una inspeccion profunda
+  app.use(logger('dev'))
+  app.use('/static', express.static(path.join(srcPath, 'public'))) // Implementacion de middleware para establecer un directorio static que alojara los archivos public para el render desde server
+  app.use('/uploads', express.static(path.join(srcPath, 'uploads'))) // Implementacion para establecer directorio estatico de las imagenes que se suben
   //* CORS
   // Config de los dominios que pueden acceder a la API
   app.use((req, res, next) => {
