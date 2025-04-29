7a40590a04d977671ee49dce8819aa90dab2c1f3
NicolasCorredor02 <nicolas.d02@outlook.com>
2025-04-28 20:48:45 -0500

    Feat  & Refeact: changeStock feature added to shopping products and code refeactored

--- src/services/productService.js
+++ src/services/productService.js
@@ -100,8 +100,10 @@ class ProductService {
             const value = paramConfig.field
 
             // Manejo de busqueda por texto para filtro de productos
-            if (paramConfig.type === 'text' && (reqQuerys.search)) {
-              const searchTerms = reqQuerys.search.split('-').map(word => word.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'))
+            if (paramConfig.type === 'text' && reqQuerys.search) {
+              const searchTerms = reqQuerys.search
+                .split('-')
+                .map((word) => word.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'))
               const orMatches = value.map((param) => ({
                 [param]: {
                   $regex: searchTerms.join('|'),
@@ -109,8 +111,9 @@ class ProductService {
                 }
               }))
               matchConditions.$or = orMatches
-            } else if (paramConfig.type === 'exact' && (reqQuerys.category)) {
-              matchConditions[paramConfig.field] = reqQuerys.category.toLowerCase()
+            } else if (paramConfig.type === 'exact' && reqQuerys.category) {
+              matchConditions[paramConfig.field] =
+                reqQuerys.category.toLowerCase()
             }
           }
         })
@@ -118,7 +121,9 @@ class ProductService {
         // Se pushean las condiciones al pipeline en caso de que se hayan encontrado por query params
         if (Object.keys(matchConditions).length > 0) {
           pipeline.push({ $match: matchConditions })
-          if (project) { pipeline.push({ ...project }) }
+          if (project) {
+            pipeline.push({ ...project })
+          }
         }
       }
 
@@ -158,10 +163,14 @@ class ProductService {
       const { body, files, deleteImages } = data
 
       // Validación del ID
-      if (!id || id.trim === '') throw new CustomError("Product's ID is required", 404)
+      if (!id || id.trim === '') {
+        throw new CustomError("Product's ID is required", 404)
+      }
 
       // Validacion si el producto viene con id para actualizar
-      if (body._id) throw new CustomError('Error, product ID can not be updated', 404)
+      if (body._id) {
+        throw new CustomError('Error, product ID can not be updated', 404)
+      }
 
       // Recuperar el producto que se quiere actualizar
       const currentProduct = await this.dao.getById(id)
@@ -170,7 +179,12 @@ class ProductService {
       // Validacion sobre el codigo para que este sea unico
       if (body.code && body.code !== currentProduct.code) {
         const existingProduct = await this.dao.codeExist({ code: body.code })
-        if (existingProduct) throw new CustomError(`The code ${body.code} is already registered`, 404)
+        if (existingProduct) {
+          throw new CustomError(
+            `The code ${body.code} is already registered`,
+            404
+          )
+        }
       }
 
       let updatedThumbnails = [...currentProduct.thumbnails] // Inicilizacion de array con las imagenes actuales
@@ -179,12 +193,19 @@ class ProductService {
       if (deleteImages && deleteImages.length > 0) {
         console.log('Images to Delte:', deleteImages)
         // Filtrar las imagenes que se van a mantener
-        updatedThumbnails = updatedThumbnails.filter(url => !deleteImages.includes(url))
+        updatedThumbnails = updatedThumbnails.filter(
+          (url) => !deleteImages.includes(url)
+        )
 
         // Eliminar de Cloudinary solo las imagenes que no son la default
-        const imagesToDeleteFromCloud = deleteImages.filter(url => url !== productUrlImageDefault)
+        const imagesToDeleteFromCloud = deleteImages.filter(
+          (url) => url !== productUrlImageDefault
+        )
         if (imagesToDeleteFromCloud.length > 0) {
-          await deleteCloudinaryImages(pathImagesProducts, imagesToDeleteFromCloud)
+          await deleteCloudinaryImages(
+            pathImagesProducts,
+            imagesToDeleteFromCloud
+          )
         }
 
         console.log('Thumbnails después de eliminar:', updatedThumbnails)
@@ -195,18 +216,24 @@ class ProductService {
         // Validacion para que no superen las 5 imagenes
         if (updatedThumbnails.length + files.length > 5) {
           // Roll back de las imagenes subidas ya que superan las 5
-          const newImagesUrls = files.map(file => file.path)
+          const newImagesUrls = files.map((file) => file.path)
           await deleteCloudinaryImages(pathImagesProducts, newImagesUrls)
 
-          throw new CustomError('A maximum of 5 images per product is allowed', 404)
+          throw new CustomError(
+            'A maximum of 5 images per product is allowed',
+            404
+          )
         }
 
         // Obtener las URLs de las nuevas imagenes
-        const newImagesUrls = files.map(file => file.path)
+        const newImagesUrls = files.map((file) => file.path)
         console.log('Nuevas imagenes a agregar:', newImagesUrls)
 
         // Si la unica imagen es la default, se remplaza con las nuevas
-        if (updatedThumbnails.length === 1 && updatedThumbnails[0] === productUrlImageDefault) {
+        if (
+          updatedThumbnails.length === 1 &&
+          updatedThumbnails[0] === productUrlImageDefault
+        ) {
           updatedThumbnails = newImagesUrls
         } else {
           // En el caso de que no, se agregan las nuevas
@@ -246,7 +273,7 @@ class ProductService {
     } catch (error) {
       // Eliminar imagenes subidas en caso de error
       const { files } = data
-      const uploadFiles = files.map(file => file.path)
+      const uploadFiles = files.map((file) => file.path)
       if (uploadFiles > 0) {
         await deleteCloudinaryImages(pathImagesProducts, uploadFiles)
       }
@@ -254,13 +281,59 @@ class ProductService {
     }
   }
 
+  changeStock = async (products) => {
+    const rollBackProducts = await Promise.all(
+      products.map((productOBj) => this.dao.getById(productOBj.product))
+    )
+
+    try {
+      for (const productObj of products) {
+        const productId = productObj.product
+        const productQuantity = productObj.quantity
+        const dbProduct = await this.dao.getById(productId)
+
+        if ((dbProduct.stock - productQuantity) < 0) {
+          throw new CustomError('Error, insufficient units in stock')
+        }
+      }
+
+      for (const productObj of products) {
+        const productId = productObj.product
+        const productQuantity = productObj.quantity
+        const dbProduct = await this.dao.getById(productId)
+
+        const productStockUpdated = await this.dao.update(productId, {
+          stock: dbProduct.stock - productQuantity
+        })
+        if (!productStockUpdated) {
+          throw new CustomError("Error, updating stock's product")
+        }
+      }
+
+      return true
+    } catch (error) {
+      try {
+        for (const product of rollBackProducts) {
+          await this.dao.update(product.id, { stock: product.stock })
+        }
+      } catch (error) {
+        console.error('Error during rollback:', error)
+      }
+      throw error
+    }
+  }
+
   changeStatus = async (id) => {
     try {
-      if (!id || id.trim === '') throw new CustomError("Product's ID is required", 404)
+      if (!id || id.trim === '') {
+        throw new CustomError("Product's ID is required", 404)
+      }
 
       const response = await this.dao.update(id, { status: false })
 
-      if (!response) throw new CustomError("Satatus's product not changed", 404)
+      if (!response) {
+        throw new CustomError("Satatus's product not changed", 404)
+      }
 
       try {
         const { _id } = await response
@@ -277,7 +350,9 @@ class ProductService {
 
   delete = async (id) => {
     try {
-      if (!id || id.trim === '') throw new CustomError("Product's ID is required", 404)
+      if (!id || id.trim === '') {
+        throw new CustomError("Product's ID is required", 404)
+      }
 
       const response = await this.dao.delete(id)
 
@@ -286,7 +361,11 @@ class ProductService {
       // Se recuperan las urls de las imagenes del producto
       const { thumbnails } = await response
 
-      if (thumbnails && thumbnails.length > 0 && (thumbnails[0] !== productUrlImageDefault)) {
+      if (
+        thumbnails &&
+        thumbnails.length > 0 &&
+        thumbnails[0] !== productUrlImageDefault
+      ) {
         // Se eliminan las imagenes que se encuentran alojadas en Cloudinary
         await deleteCloudinaryImages(pathImagesProducts, thumbnails)
       }
