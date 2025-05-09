7c47826ba9140a20ce62c7243812952bc51bdfe1
NicolasCorredor02 <nicolas.d02@outlook.com>
2025-05-09 00:00:07 -0500

    Feat: Principal route added

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,19 @@ import products from 'root/routes/product.route.js'
 import carts from 'root/routes/cart.route.js'
 import admin from 'root/routes/admin.routes.js'
 import user from 'root/routes/user.route.js'
-import { productController } from 'root/controller/user/product.controller.js'
 
 const router = Router()
 
 /**
  * Get de Home Page
  */
-router.get('/', productController.getAll)
+router.get('/', (req, res, next) => {
+  try {
+    return res.render('productsClient')
+  } catch (error) {
+    next(error)
+  }
+})
 
 /**
  * Get para rutas no existentes
